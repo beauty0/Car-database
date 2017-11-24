27a278f9f8557bdf9e8a63e21e1ee7370bee6821
beauty0 <isigirluk@yahoo.co.uk>
2017-11-24 14:50:17 -0500

    FETCH ADDRESS CURSOR
    I created a cursor to fetch the customer's address from the carcust table, then l created a loop  to fetch the record. With the use of carcust _id  then l could fetch the customer name and address.

---
+++ Myfirst cursor.sql
@@ -0,0 +1,26 @@
+set SERVEROUTPUT ON;
+-----create cursor----
+DECLARE
+ Cursor cur_carcu
+ is 
+ ------giving it a query of last_name and address from CARCUST table
+ select LAST_NAME, ADDRESS FROM CARCUST WHERE CARCUST_ID =&CARCUST_ID;
+ ----declaring 2 variables----
+ Vln CARCUST.LAST_NAME%TYPE;
+ Vad CARCUST.ADDRESS%TYPE;
+ -----End of declare section---- 
+ BEGIN
+ -----opening the cursor----
+  OPEN cur_carcu;
+  -----to fetch the record using loop-----
+  loop
+    fetch cur_carcu into Vln,Vad;
+    EXIT WHEN cur_carcu%notfound;
+    -----to diplay the name and address----
+DBMS_OUTPUT.PUT_LINE(Vln||'-'||Vad);
+DBMS_OUTPUT.PUT_LINE(cur_carcu%rowcount);
+END LOOP;
+ Close cur_carcu;
+ end;
+    
+ 
